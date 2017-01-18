9faac6d41df5e74eac68749f6ff200235154e4fc
Pedro Santos <milheiro.santos@gmail.com>
2017-01-18 20:58:31 +0100

    changed onhover effect; add effect to disabled buttons; code cleanup

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
+//============================================================================
+//  Gallery 
+//============================================================================
+
 var active_element = null;
-
-
 function current_gallery_element(){
   return active_element;
 }
@@ -25,13 +27,28 @@ function set_gallery_image(entry_element){
 function set_gallery_video(entry_element){
   var video_element = document.getElementById("gallery-video");
   video_element.style.display = "block";
-  set_gallery_video_size(video_element);
   video_element.src = entry_element.getAttribute("link");
+}
+
+function disable_button(element){
+  element.classList.remove("osd-button");
+  element.classList.add("disabled-osd-button");
+}
+
+function enable_button(element){
+  element.classList.remove("disabled-osd-button");
+  element.classList.add("osd-button");
 }
 
 function set_gallery(entry_element){
   var video_gallery = document.getElementById("gallery-video");
   var image_gallery = document.getElementById("gallery-image");
+
+  var prevButton = document.getElementById("gallery-previous");
+  var nextButton = document.getElementById("gallery-next");
+
+  enable_button(prevButton);
+  enable_button(nextButton);
 
   video_gallery.style.display = "none";
   image_gallery.style.display = "none";
@@ -49,41 +66,50 @@ function set_gallery(entry_element){
     video_gallery.style.display = "block";
   }
 
-}
+  //Now that we changed the gallery element, 
+  //we check if the buttons should be enabled
+  var next = next_gallery_element();
+  if(!next){
+    disable_button(nextButton);
+  }
+  var prev = previous_gallery_element();
+  if(!prev){
+    disable_button(prevButton);
+  }
 
-function set_background_video_size(video_element){
-  var aspectRatio = 9./16.;
-  var height = document.querySelector("body").offsetHeight;
-  var width = height/aspectRatio;
-  video_element.style.width= "" + width  + "px";
-  video_element.style.height= "" + height + "px";
-}
-
-function set_gallery_video_size(video_element){
-//  var aspectRatio = 9./16.;
-//  var width = document.querySelector("body").offsetWidth*.9;
-//  var height = width*aspectRatio;
-//  video_element.style.width= "" + width  + "px";
-//  video_element.style.height= "" + height + "px";
 }
 
 function next_gallery_element(){
   var next = current_gallery_element().nextElementSibling;
   if (next && next.classList.contains("entry")){
-    set_gallery(next);
+    return next;
   }
 }
 
 function previous_gallery_element(){
   var prev = current_gallery_element().previousElementSibling;
-    if (prev && prev.classList.contains("entry")){
+  if (prev && prev.classList.contains("entry")){
+    return prev;
+  }
+}
+
+function set_next_gallery_element(){
+  var next = next_gallery_element();
+  if (next) {
+    set_gallery(next);
+  }
+}
+
+function set_previous_gallery_element(){
+  var prev = previous_gallery_element();
+  if (prev){
     set_gallery(prev);
   }
 }
 
 function open_gallery(element){
   set_gallery(element); 
-  var title_el = document.querySelector("#gallery-overlay .gallery-header h1");
+  var title_el = document.querySelector("#gallery-overlay .osd-header h1");
   title_el.innerHTML = element.parentElement.getAttribute("name");
   document.getElementById("gallery-overlay").style.display = "block";
 }
@@ -92,24 +118,34 @@ function close_gallery(){
   document.getElementById("gallery-overlay").style.display = "none";
 }
 
-document.addEventListener("DOMContentLoaded", function(event) { 
-  var entries = document.querySelectorAll("div.album");
-  for(var i=0; i<entries.length; ++i){
-    var img = document.createElement("img");
-    img.src = entries[i].getAttribute("thumb");
-    entries[i].appendChild(img);
-    entries[i].addEventListener("click", function(ev){
-      open_gallery(ev.currentTarget.firstElementChild);
-    });
+//============================================================================
+// Resize background video to fill screen 
+//============================================================================
+
+function set_background_video_size(video_element){
+  var aspectRatio = 9./16.;
+  var screenHeight = document.querySelector("body").offsetHeight;
+  var screenWidth = document.querySelector("body").offsetWidth;
+
+  var height = screenHeight;
+  var width = height/aspectRatio;
+  if(width < screenWidth)
+  {
+    width = screenWidth;
+    height = width*aspectRatio;
   }
 
-  set_background_video_size(document.getElementById("background_video"))
-});
+  video_element.style.width = "" + width  + "px";
+  video_element.style.height = "" + height + "px";
+}
 
 window.addEventListener('resize', function(event){
   set_background_video_size(document.getElementById("background_video"));
-  set_gallery_video_size(document.getElementById("gallery-video"));
 });
+
+//============================================================================
+//  OSD transitions 
+//============================================================================
 
 function close_osd(element, callback) {
   var old_height = element.offsetHeight; 
@@ -150,9 +186,26 @@ function open_osd(element, callback) {
   }
 }
 
-
 function change_osd(from, to){
   var from_el = document.getElementById(from);
   var to_el = document.getElementById(to);
   close_osd(from_el, function(){open_osd(to_el);});
 }
+
+//============================================================================
+//  Startup
+//============================================================================
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+  var entries = document.querySelectorAll("div.album");
+  for(var i=0; i<entries.length; ++i){
+    var img = document.createElement("img");
+    img.src = entries[i].getAttribute("thumb");
+    entries[i].appendChild(img);
+    entries[i].addEventListener("click", function(ev){
+      open_gallery(ev.currentTarget.firstElementChild);
+    });
+  }
+
+  set_background_video_size(document.getElementById("background_video"))
+});
